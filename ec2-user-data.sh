ae0932e1a8cc076cb3be71305dbd5bfcd1f07152
Ueslei <ueslei@outlook.com>
2024-05-18 21:53:33 -0300

    Update EC2 creation and Streamlit deployment with environment secrets

--- ec2-user-data.sh
+++ ec2-user-data.sh
@@ -6,3 +6,4 @@ sudo pip3 install boto3 opencv-python-headless python-dotenv streamlit fastapi u
 git clone https://github.com/uesleisutil/liveness_detection.git /home/ec2-user/liveness_detection
 cd /home/ec2-user/liveness_detection
 nohup streamlit run app/streamlit_app.py --server.port 80 --server.address 0.0.0.0 &
+EOF
