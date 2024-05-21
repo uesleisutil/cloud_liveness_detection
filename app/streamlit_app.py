08423a270091b376c03efe517cda38ae7283b40b
Ueslei <ueslei@outlook.com>
2024-05-21 00:08:09 -0300

    Final changes...

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -4,7 +4,6 @@ import tempfile
 from utils import upload_to_s3, detect_faces_in_video, clear_s3_bucket
 import streamlit.components.v1 as components
 
-# Function to handle the uploaded video file
 def handle_uploaded_video(video_file):
     tempdir = tempfile.mkdtemp()
     video_path = os.path.join(tempdir, "uploaded_video.webm")
@@ -16,67 +15,65 @@ def handle_uploaded_video(video_file):
 
 # HTML and JavaScript for video capture
 video_html = """
-    <video id="video" width="640" height="480" autoplay></video>
-    <button id="startButton">Start Recording</button>
-    <button id="stopButton" disabled>Stop Recording</button>
-    <script>
-        let mediaRecorder;
-        let recordedBlobs;
+    <div>
+        <video id="video" width="640" height="480" autoplay muted></video>
+        <div>
+            <button id="startButton">Start Recording</button>
+            <button id="stopButton" disabled>Stop Recording</button>
+        </div>
+        <script>
+            let mediaRecorder;
+            let recordedBlobs;
 
-        const video = document.querySelector('video');
-        const startButton = document.getElementById('startButton');
-        const stopButton = document.getElementById('stopButton');
+            const video = document.querySelector('video');
+            const startButton = document.getElementById('startButton');
+            const stopButton = document.getElementById('stopButton');
 
-        startButton.addEventListener('click', async () => {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-            video.srcObject = stream;
-            recordedBlobs = [];
-            const options = { mimeType: 'video/webm;codecs=vp9' };
-            mediaRecorder = new MediaRecorder(stream, options);
+            startButton.addEventListener('click', async () => {
+                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+                video.srcObject = stream;
+                recordedBlobs = [];
+                const options = { mimeType: 'video/webm;codecs=vp9' };
+                mediaRecorder = new MediaRecorder(stream, options);
 
-            mediaRecorder.onstop = async (event) => {
-                const blob = new Blob(recordedBlobs, { type: 'video/webm' });
-                const formData = new FormData();
-                formData.append('file', blob, 'recorded.webm');
+                mediaRecorder.onstop = (event) => {
+                    const blob = new Blob(recordedBlobs, { type: 'video/webm' });
+                    const url = window.URL.createObjectURL(blob);
+                    const a = document.createElement('a');
+                    a.style.display = 'none';
+                    a.href = url;
+                    a.download = 'test.webm';
+                    document.body.appendChild(a);
+                    a.click();
+                    window.URL.revokeObjectURL(url);
+                };
 
-                const response = await fetch('/upload_video', {
-                    method: 'POST',
-                    body: formData
-                });
+                mediaRecorder.ondataavailable = (event) => {
+                    if (event.data && event.data.size > 0) {
+                        recordedBlobs.push(event.data);
+                    }
+                };
 
-                if (response.ok) {
-                    const data = await response.json();
-                    console.log('Success:', data);
-                } else {
-                    console.error('Upload failed.');
-                }
-            };
+                mediaRecorder.start();
+                startButton.disabled = true;
+                stopButton.disabled = false;
+            });
 
-            mediaRecorder.ondataavailable = (event) => {
-                if (event.data && event.data.size > 0) {
-                    recordedBlobs.push(event.data);
-                }
-            };
-
-            mediaRecorder.start();
-            startButton.disabled = true;
-            stopButton.disabled = false;
-        });
-
-        stopButton.addEventListener('click', () => {
-            mediaRecorder.stop();
-            video.srcObject.getTracks().forEach(track => track.stop());
-            startButton.disabled = false;
-            stopButton.disabled = true;
-        });
-    </script>
+            stopButton.addEventListener('click', () => {
+                mediaRecorder.stop();
+                video.srcObject.getTracks().forEach(track => track.stop());
+                startButton.disabled = false;
+                stopButton.disabled = true;
+            });
+        </script>
+    </div>
 """
 
 def main():
     st.title("Quantum Finance - Facial Liveness Detection")
     st.write("Click the button below to capture a video and verify liveness.")
 
-    components.html(video_html)
+    components.html(video_html, height=600)
 
     uploaded_video = st.file_uploader("Upload your recorded video", type=["webm"])
     if uploaded_video is not None:
