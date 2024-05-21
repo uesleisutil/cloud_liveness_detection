f1a04612cd8a35b1652559ba79d72b2b88440515
Ueslei <ueslei@outlook.com>
2024-05-21 19:40:19 -0300

    Final changes...

--- app/api.py
+++ app/api.py
@@ -1,9 +1,78 @@
 import streamlit as st
 import os
 import tempfile
-from utils import upload_to_s3, detect_faces_in_video, clear_s3_bucket
 import streamlit.components.v1 as components
+import boto3
+import os
+from dotenv import load_dotenv
+import cv2
 
+
+
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+REGION_NAME = os.getenv('AWS_REGION')
+BUCKET_NAME = os.getenv('S3_BUCKET')
+
+rekognition = boto3.client('rekognition', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name='us-east-1')
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name='us-east-1')
+
+def clear_s3_bucket():
+    try:
+        bucket = s3.list_objects_v2(Bucket=BUCKET_NAME)
+        if 'Contents' in bucket:
+            for item in bucket['Contents']:
+                s3.delete_object(Bucket=BUCKET_NAME, Key=item['Key'])
+        print("All images deleted from S3 bucket.")
+    except Exception as e:
+        print(f"Error deleting images from S3: {e}")
+
+def upload_to_s3(filename):
+    try:
+        s3.upload_file(filename, BUCKET_NAME, os.path.basename(filename))
+        return os.path.basename(filename)
+    except Exception as e:
+        print(f"Error uploading file to S3: {e}")
+        return None
+
+def detect_faces(filename):
+    try:
+        response = rekognition.detect_faces(
+            Image={'S3Object': {'Bucket': BUCKET_NAME, 'Name': filename}},
+            Attributes=['ALL']
+        )
+        return response
+    except Exception as e:
+        print(f"Error detecting faces: {e}")
+        return None
+
+def analyze_movement(images):
+    if len(images) < 2:
+        return False
+
+    img1 = cv2.imread(images[0], cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(images[1], cv2.IMREAD_GRAYSCALE)
+
+    diff = cv2.absdiff(img1, img2)
+    non_zero_count = np.count_nonzero(diff)
+
+    threshold = 5000
+    return non_zero_count > threshold
+
+def detect_faces_in_video(filename):
+    try:
+        response = rekognition.detect_faces(
+            Video={'S3Object': {'Bucket': BUCKET_NAME, 'Name': filename}},
+            Attributes=['ALL']
+        )
+        return response
+    except Exception as e:
+        print(f"Error detecting faces: {e}")
+        return None
+    
+    
 def handle_uploaded_video(video_file):
     tempdir = tempfile.mkdtemp()
     video_path = os.path.join(tempdir, "uploaded_video.webm")
