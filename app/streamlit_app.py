bb7964bb97e01c2250bb3b70f41ddace752d5131
Ueslei <ueslei@outlook.com>
2024-05-21 18:46:39 -0300

    Final changes...

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit as st
-import streamlit.components.v1 as components
 import os
 import tempfile
 from utils import upload_to_s3, detect_faces_in_video, clear_s3_bucket
+import streamlit.components.v1 as components
 from dotenv import load_dotenv
 
 load_dotenv()
+
+DOMAIN_NAME = 'cloud_liveness_fiap.com' 
 
 def handle_uploaded_video(video_file):
     tempdir = tempfile.mkdtemp()
@@ -16,14 +18,13 @@ def handle_uploaded_video(video_file):
 
     return video_path, tempdir
 
-# HTML and JavaScript for video capture with permission request
+# HTML and JavaScript for video capture
 video_html = """
 <div>
     <video id="video" width="640" height="480" autoplay muted></video>
     <div>
         <button id="startButton">Start Recording</button>
         <button id="stopButton" disabled>Stop Recording</button>
-        <a id="downloadLink" style="display: none;">Download Video</a>
     </div>
     <script>
         let mediaRecorder;
@@ -32,72 +33,64 @@ video_html = """
         const video = document.querySelector('video');
         const startButton = document.getElementById('startButton');
         const stopButton = document.getElementById('stopButton');
-        const downloadLink = document.getElementById('downloadLink');
 
-        async function requestCameraPermission() {
-            try {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-                return stream;
-            } catch (error) {
-                console.error("Error accessing media devices.", error);
-                alert("Permission to access the camera was denied. Please allow camera access and try again.");
-                throw error;
-            }
-        }
-
-        startButton.addEventListener('click', async () => {
+        startButton.addEventListener('click', async () => {{
             console.log("Start button clicked");
-            try {
-                const stream = await requestCameraPermission();
+            try {{
+                const stream = await navigator.mediaDevices.getUserMedia({{ video: true }});
                 console.log("Media stream obtained", stream);
                 video.srcObject = stream;
                 recordedBlobs = [];
-                const options = { mimeType: 'video/webm;codecs=vp9' };
+                const options = {{ mimeType: 'video/webm;codecs=vp9' }};
                 mediaRecorder = new MediaRecorder(stream, options);
 
-                mediaRecorder.onstop = async (event) => {
+                mediaRecorder.onstop = async (event) => {{
                     console.log("Recording stopped", event);
-                    const blob = new Blob(recordedBlobs, { type: 'video/webm' });
+                    const blob = new Blob(recordedBlobs, {{ type: 'video/webm' }});
                     const url = window.URL.createObjectURL(blob);
-                    downloadLink.href = url;
-                    downloadLink.download = 'recorded_video.webm';
-                    downloadLink.style.display = 'block';
-                    
+                    const a = document.createElement('a');
+                    a.style.display = 'none';
+                    a.href = url;
+                    a.download = 'test.webm';
+                    document.body.appendChild(a);
+                    a.click();
+                    window.URL.revokeObjectURL(url);
+
                     const formData = new FormData();
                     formData.append('file', blob, 'recorded.webm');
 
-                    fetch('http://your-ec2-public-ip:8000/upload_video', {
+                    fetch('http://your-ec2-public-ip:8000/upload_video', {{
                         method: 'POST',
                         body: formData
-                    }).then(response => response.json())
+                    }}).then(response => response.json())
                       .then(data => console.log('Success:', data))
                       .catch(error => console.error('Error:', error));
-                };
+                }};
 
-                mediaRecorder.ondataavailable = (event) => {
-                    if (event.data && event.data.size > 0) {
+                mediaRecorder.ondataavailable = (event) => {{
+                    if (event.data && event.data.size > 0) {{
                         recordedBlobs.push(event.data);
-                    }
-                };
+                    }}
+                }};
 
                 mediaRecorder.start();
                 console.log("MediaRecorder started", mediaRecorder);
                 startButton.disabled = true;
                 stopButton.disabled = false;
-            } catch (error) {
-                console.error("Error starting video recording:", error);
-            }
-        });
+            }} catch (error) {{
+                console.error("Error accessing media devices.", error);
+            }}
+        }});
 
-        stopButton.addEventListener('click', () => {
+        stopButton.addEventListener('click', () => {{
             mediaRecorder.stop();
             video.srcObject.getTracks().forEach(track => track.stop());
             startButton.disabled = false;
             stopButton.disabled = true;
-        });
+        }});
     </script>
 </div>
-"""
+""".format(DOMAIN_NAME)
 
 def main():
     st.title("Quantum Finance - Facial Liveness Detection")
