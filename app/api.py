37415a1588cb195529dffa03e6935009ab8a59a0
Ueslei <ueslei@outlook.com>
2024-05-20 22:03:07 -0300

    Final changes...

--- app/api.py
+++ app/api.py
@@ -12,9 +12,6 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION')
 BUCKET_NAME = os.getenv('S3_BUCKET')
 
-if not AWS_DEFAULT_REGION:
-    raise ValueError("AWS_DEFAULT_REGION environment variable not set")
-
 rekognition = boto3.client('rekognition', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_DEFAULT_REGION)
 s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_DEFAULT_REGION)
 
--- app/utils.py
+++ app/utils.py
@@ -9,9 +9,6 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_DEFAULT_REGION = os.getenv('AWS_DEFAULT_REGION')
 BUCKET_NAME = os.getenv('S3_BUCKET')
 
-if not AWS_ACCESS_KEY_ID or not AWS_SECRET_ACCESS_KEY or not AWS_DEFAULT_REGION or not BUCKET_NAME:
-    raise ValueError("AWS environment variables not set properly")
-
 rekognition = boto3.client('rekognition', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_DEFAULT_REGION)
 s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name=AWS_DEFAULT_REGION)
 
